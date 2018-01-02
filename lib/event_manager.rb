250c06ba08ff6f1d777a2c9d6d3b886c38e80aa1
Scott Fox <scottfox87@gmail.com>
2018-01-02 17:35:28 -0600

    convert headers to symbols

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,8 +3,8 @@ puts "EventManager Initialized!"
 require "csv"
 puts "EventManager initialized."
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
+  name = row[:first_name]
   puts name
 end
