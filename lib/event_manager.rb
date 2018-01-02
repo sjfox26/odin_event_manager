e21a7f08a2c5d7eb2cec5fbffe08996428eb51b4
Scott Fox <scottfox87@gmail.com>
2018-01-02 17:36:22 -0600

    display first names along with zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,5 +6,6 @@ puts "EventManager initialized."
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  puts name
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
