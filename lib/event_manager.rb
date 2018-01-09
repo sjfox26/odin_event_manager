dce47a60274c6175d1f0e5e74f0786761ea138b6
Scott Fox <scottfox87@gmail.com>
2018-01-09 15:34:29 -0600

    Add clean phone number functionality

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,21 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone(phone_number)
+  only_numbers = phone_number.gsub(/\W\s?/, "")
+  digit_array = only_numbers.split("")
+  num_digits = digit_array.length
+  if num_digits == 10
+    return only_numbers
+  elsif num_digits == 11 && digit_array[0] == '1'
+    digit_array.shift
+    number_with_no_leading_1 = digit_array.join("")
+    return number_with_no_leading_1
+  else
+    "user entered invalid phone number"
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -35,16 +50,19 @@ puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.erb"
-erb_template = ERB.new template_letter
+#template_letter = File.read "form_letter.erb"
+#erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
+  phone = clean_phone(row[:homephone])
 
-  form_letter = erb_template.result(binding)
+  puts "#{name} #{zipcode} #{phone}"
+#  legislators = legislators_by_zipcode(zipcode)
 
-  save_thank_you_letters(id,form_letter)
+#  form_letter = erb_template.result(binding)
+
+#  save_thank_you_letters(id,form_letter)
 end
